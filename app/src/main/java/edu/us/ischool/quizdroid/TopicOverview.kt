714b6c473e281eca1f5f10d28f2d069caff92cea
ari-apigo <ari.apigo125@gmail.com>
2022-02-02 14:25:35 -0800

    first 3 stories

--- app/src/main/java/edu/us/ischool/quizdroid/TopicOverview.kt
+++ app/src/main/java/edu/us/ischool/quizdroid/TopicOverview.kt
@@ -1,8 +1,10 @@
 package edu.us.ischool.quizdroid
 
+import android.content.Intent
 import android.content.Intent.EXTRA_TEXT
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class TopicOverview : AppCompatActivity() {
@@ -16,6 +18,15 @@ class TopicOverview : AppCompatActivity() {
         // Capture the layout's TextView and set the string as its text
         val quizTopicTV = findViewById<TextView>(R.id.tvTopic).apply {
             text = message
+        }
+
+        // get begin button reference
+        val btnBegin = findViewById<Button>(R.id.btnBeginQuiz)
+        // start quiz question activity on click
+        btnBegin.setOnClickListener{
+            //val quizTopic = quizTopicTV.text.toString()
+            val intent = Intent(this, QuizQuestion::class.java)
+            startActivity(intent)
         }
     }
 }
