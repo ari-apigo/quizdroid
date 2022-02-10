50a8e3f4b09d528985c0fb7dd2febd175852b87f
ari-apigo <ari.apigo125@gmail.com>
2022-02-09 21:59:45 -0800

    reroute preferences activity

--- app/src/main/java/edu/us/ischool/quizdroid/Preferences.kt
+++ app/src/main/java/edu/us/ischool/quizdroid/Preferences.kt
@@ -1,11 +1,18 @@
 package edu.us.ischool.quizdroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Preferences : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+
+        findViewById<Button>(R.id.btnUpdatePref).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
